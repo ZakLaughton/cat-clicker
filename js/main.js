c54f30616f23b39772fad10098f6349678209836
Zak Laughton <zlaughton@gmail.com>
2018-07-09 10:28:57 -0500

    Made MV* model work!!!

--- js/main.js
+++ js/main.js
@@ -1,66 +1,117 @@
-$(function(){
-    let data = {
+(function(){
+    let model = {
+        currentCat: null,
         cats: [
-            {name: 'Ross', image: 'cat1.jpg'},
-            {name: 'Rachel', image: 'cat2.jpg'},
-            {name: 'Joey', image: 'cat3.jpg'},
-            {name: 'Pheobe', image: 'cat4.jpg'},
-            {name: 'Monica', image: 'cat5.jpg'},
-            {name: 'Chandler', image: 'cat6.jpg'}
+            {
+                name: 'Ross',
+                image: 'cat1.jpg',
+                clickCount: 0
+            },
+            {
+                name: 'Rachel',
+                image: 'cat2.jpg',
+                clickCount: 0
+            },
+            {
+                name: 'Joey',
+                image: 'cat3.jpg',
+                clickCount: 0
+            },
+            {
+                name: 'Pheobe',
+                image: 'cat4.jpg',
+                clickCount: 0
+            },
+            {
+                name: 'Monica',
+                image: 'cat5.jpg',
+                clickCount: 0
+            },
+            {
+                name: 'Chandler',
+                image: 'cat6.jpg',
+                clickCount: 0
+            }
         ]
     };
 
     let octopus = {
         init: function() {
-            view.init();
+            // Set current cat to the first cat
+            model.currentCat = model.cats[0];
+
+            // Initialize views
+            catListView.init();
+            catView.init();
+        },
+
+        getCurrentCat: function() {
+            return model.currentCat;
+        },
+
+        getCats: function() {
+            return model.cats;
+        },
+
+        setCurrentCat: function(cat) {
+            model.currentCat = cat;
+        },
+
+        incrementCounter: function() {
+            model.currentCat.clickCount++;
+            catView.render();
         }
     }
 
-    let view = {
+    let catView = {
         init: function() {
-            this.name = $('#cat-name') 
-            this.clickCount = $('#click-count') 
-            this.clickerImage = $('#clicker-image') 
-            view.render();
-            octopus.getCatInfo()
+            this.catElem = $('#cat');
+            this.nameElem = $('#cat-name');
+            this.countElem = $('#click-count');
+            this.catImageElem = $('#clicker-image');
 
-        }
+            this.catImageElem.click(function() {
+                octopus.incrementCounter();
+            })
+
+            this.render();
+        },
 
         render: function() {
+            // Update DOM with elements for current cat
+            let currentCat = octopus.getCurrentCat();
+            this.countElem.text(currentCat.clickCount);
+            this.nameElem.text(currentCat.name);
+            this.catImageElem.attr('src', `img/${currentCat.image}`);
+        }
+    }
+
+    let catListView = {
+        init: function() {
+            this.catListElem = $('#cat-list');
+            this.render();
+        },
+
+        render: function() {
+            let cats = octopus.getCats();
+            this.catListElem.innerHTML = '';
+
+            for (const [index, cat] of cats.entries()) {
+                let elem = document.createElement('li');
+                elem.textContent = cat.name;
+
+                elem.addEventListener('click', (function(cat) {
+                    return function () {
+                        octopus.setCurrentCat(cat);
+                        catView.render();
+                    }
+                })(cat));
+
+                this.catListElem.append(elem);
+            }
         }
     }
 
     octopus.init();
 })();
-
-cats.forEach(function(cat, index) {
-    // Add cat to list
-    catListItem = $('<li/>', {
-        class: `cat-name cat${index}`,
-        html: cat.name
-    })
-    $('#cat-list').append(catListItem)
-    $(`.cat-name.cat${index}`).click((function(index) {
-        return function () {
-            $(`main div.active`).removeClass('active').addClass('hidden')
-            $(`main div.cat.cat${index}`).removeClass('hidden').addClass('active')
-        }
-    })(index))
-    
-    // Add cat to main
-    $('<div/>', {
-        class: `hidden cat cat${index}`,
-        html: catHtml
-    }).appendTo('main')
-
-    $(`.cat${index}`).children('.clicker-cat').click((function(indexCopy) {
-        return function() {
-            let cat = $(`.cat${index}`)
-            let clickCount = Number(cat.find('.click-count').text())
-            clickCount += 1;
-            cat.find('.click-count').text(clickCount);
-        }
-    })(index))
-    
-})
 
